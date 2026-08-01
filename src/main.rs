b59d594e43adb5a37400a3328721701ebdaac7d6
Sanju Sabu <sanjusabu.github@gmail.com>
2026-08-01 19:38:34 +0530

    prototype: add the function that checks whether the "ip" command is installed or not

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::process::Command;
+
 fn main() {
-    println!("Prototype branch");
+    if ip_command_exists() {
+        println!("Success");
+    } else {
+        println!("Error");
+    }
 }
+
+// Keep
+fn ip_command_exists() -> bool {
+    Command::new("ip")
+        .arg("-V")
+        .output()
+        .is_ok_and(|output| output.status.success())
+}
+
+// The logic should be
+// If the above function returns true, run the function below 
+// else just return a string that says "dependencies not installed" or something like that
