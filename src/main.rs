c0baa41f84919c9baff2f31fa30d24bbaf8f16c6
Sanju Sabu <sanjusabu.github@gmail.com>
2026-08-02 20:36:06 +0530

    feat: add function that finds the broadcast address of the network

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::process::Command;
 
 fn main() {
-    println!("{:?}", get_network_interface());
+    if ip_command_exists() {
+        if let Some(v) = get_network_interface() {
+            println!("{:?}", get_broadcast_addr(&v));
+        }
+    }
 }
 
 // Keep
@@ -16,7 +20,7 @@ fn ip_command_exists() -> bool {
 // If the above function returns true, run the function below
 // else just return a string that says "dependencies not installed" or something like that
 
-// Keep this
+// Keep 
 fn get_network_interface() -> Option<String> {
     if let Ok(output) = Command::new("ip")
         .args(["route", "get", "8.8.8.8"])
@@ -30,6 +34,29 @@ fn get_network_interface() -> Option<String> {
             }
         }
     }
-    
+
+    None
+}
+
+// Keep
+fn get_broadcast_addr(interface: &str) -> Option<String> {
+    if let Ok(output) = Command::new("ip")
+        .args(["address", "show", interface])
+        .output()
+    {
+        let text = String::from_utf8_lossy(&output.stdout);
+        for line in text.lines() {
+            let mut words = line.split_whitespace();
+            while let Some(word) = words.next() {
+                if word == "inet" {
+                    words.next();
+                    if words.next() == Some("brd") {
+                        return words.next().map(str::to_string);
+                    }
+                }
+            }
+        }
+    }
+
     None
 }
