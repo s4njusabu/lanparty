e61fba925e1366ff895d2564741221990aad75c7
Sanju Sabu <sanjusabu.github@gmail.com>
2026-08-04 20:59:24 +0530

    feat(client): add chat window prototype

--- src/ui/border.rs
+++ src/ui/border.rs
@@ -1,7 +1,7 @@
 use ratatui::{
     Frame,
     layout::Rect,
-    style::{Modifier, Style},
+    style::{Modifier, Style, Stylize},
     text::{Line, Span},
     widgets::{Block, BorderType},
 };
@@ -11,20 +11,23 @@ use crate::app::state::State;
 pub fn draw_border(frame: &mut Frame, state: &State) -> Rect {
     let colors = state.theme.colors();
     let block = Block::bordered()
-        .title(Line::from(vec![
-            Span::styled(
-                " LAN",
-                Style::default()
-                    .fg(colors.text)
-                    .add_modifier(Modifier::BOLD),
-            ),
-            Span::styled(
-                "Party ",
-                Style::default()
-                    .fg(colors.text)
-                    .add_modifier(Modifier::BOLD),
-            ),
-        ]))
+        // .title(Line::from(vec![
+        //     Span::styled(
+        //         " LAN",
+        //         Style::default()
+        //             .fg(colors.text)
+        //             .add_modifier(Modifier::BOLD),
+        //     ),
+        //     Span::styled(
+        //         "Party ",
+        //         Style::default()
+        //             .fg(colors.text)
+        //             .add_modifier(Modifier::BOLD),
+        //     ),
+        // ]))
+        .title(Line::from(
+            Span::styled(" LANParty ", colors.text).add_modifier(Modifier::BOLD),
+        ))
         .border_type(BorderType::Thick)
         .border_style(Style::default().fg(colors.accent));
 
