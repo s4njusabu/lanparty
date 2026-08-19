a440a81c7d8c1a5ede26ac050be25c28afff02a0
Sanju Sabu <sanjusabu.dev@gmail.com>
2026-08-20 00:25:43 +0530

    Add themes to the new version

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ use crate::services::username;
 mod states;
 mod services;
 mod ui;
+pub mod themes;
 
 fn main() {
     let terminal = ratatui::init();
     let mut username = username::default_username();
 
-    loop {
-        
-    }
+    // loop {
+
+    // }
 
     ratatui::restore();
     println!("Bye from LAN Party!");
