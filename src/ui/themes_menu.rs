86b6ea46a428dd4cbf09faad08eb139729f8db36
Sanju Sabu <sanjusabu.github@gmail.com>
2026-08-20 20:29:19 +0530

    Add the modified themes_menu.rs

--- src/ui/themes_menu.rs
+++ src/ui/themes_menu.rs
@@ -7,7 +7,7 @@ use ratatui::{
 
 use crate::states::ui_state::UiState;
 
-pub const THEME_OPTIONS_MAX_INDEX: usize = 1;
+pub const THEME_OPTIONS_MAX_INDEX: usize = 2;
 
 pub fn draw_themes_menu(frame: &mut Frame, inner: Rect, ui_state: &UiState) {
     let colors = ui_state.theme.colors();
@@ -23,80 +23,177 @@ pub fn draw_themes_menu(frame: &mut Frame, inner: Rect, ui_state: &UiState) {
 
     draw_banner(frame, title, ui_state);
 
-    let [dark_row, light_row] = Layout::vertical([Constraint::Length(3), Constraint::Length(3)])
-        .spacing(1)
-        .areas(options);
+    let [dark_area, light_area, back_area] = Layout::vertical([
+        Constraint::Length(5),
+        Constraint::Length(5),
+        Constraint::Length(5),
+    ])
+    .spacing(1)
+    .areas(options);
+
+    let [dark, dark_description] =
+        Layout::vertical([Constraint::Length(3), Constraint::Length(2)]).areas(dark_area);
+
+    let [light, light_description] =
+        Layout::vertical([Constraint::Length(3), Constraint::Length(2)]).areas(light_area);
+
+    let [back, back_description] =
+        Layout::vertical([Constraint::Length(3), Constraint::Length(2)]).areas(back_area);
+
     let [dark] = Layout::horizontal([Constraint::Length(22)])
         .flex(Flex::Center)
-        .areas(dark_row);
+        .areas(dark);
+
     let [light] = Layout::horizontal([Constraint::Length(22)])
         .flex(Flex::Center)
-        .areas(light_row);
+        .areas(light);
+
+    let [back] = Layout::horizontal([Constraint::Length(22)])
+        .flex(Flex::Center)
+        .areas(back);
 
     let text_style = Style::default()
         .fg(colors.text)
         .add_modifier(Modifier::BOLD);
+
+    let description_style = Style::default().fg(colors.text);
+
     let border_style = Style::default().fg(colors.accent);
 
-    let block = Block::bordered()
+    // Dark
+    let dark_block = Block::bordered()
         .border_type(BorderType::Double)
-        .border_style(border_style);
+        .border_style(if ui_state.submenu_hovered == Some(0) {
+            Style::default()
+                .fg(colors.selected)
+                .add_modifier(Modifier::BOLD)
+        } else {
+            border_style
+        });
+
+    frame.render_widget(dark_block.clone(), dark);
+
+    let dark_inner = dark_block.inner(dark);
 
     frame.render_widget(
         Paragraph::new("Dark")
             .style(text_style)
-            .alignment(Alignment::Center)
-            .block(block.clone()),
-        dark,
+            .alignment(Alignment::Center),
+        dark_inner,
     );
 
     if ui_state.submenu_hovered == Some(0) {
-        let inner = block.inner(dark);
-
         frame.render_widget(
             Paragraph::new("»")
                 .style(text_style)
                 .alignment(Alignment::Left),
-            inner,
+            dark_inner,
         );
 
         frame.render_widget(
             Paragraph::new("«")
                 .style(text_style)
                 .alignment(Alignment::Right),
-            inner,
+            dark_inner,
         );
     }
 
-    let block = Block::bordered()
+    frame.render_widget(
+        Paragraph::new("Default theme")
+            .style(description_style)
+            .alignment(Alignment::Center),
+        dark_description,
+    );
+
+    // Light
+    let light_block = Block::bordered()
         .border_type(BorderType::Double)
-        .border_style(border_style);
+        .border_style(if ui_state.submenu_hovered == Some(1) {
+            Style::default()
+                .fg(colors.selected)
+                .add_modifier(Modifier::BOLD)
+        } else {
+            border_style
+        });
+
+    frame.render_widget(light_block.clone(), light);
+
+    let light_inner = light_block.inner(light);
 
     frame.render_widget(
         Paragraph::new("Light")
             .style(text_style)
-            .alignment(Alignment::Center)
-            .block(block.clone()),
-        light,
+            .alignment(Alignment::Center),
+        light_inner,
     );
 
     if ui_state.submenu_hovered == Some(1) {
-        let inner = block.inner(light);
-
         frame.render_widget(
             Paragraph::new("»")
                 .style(text_style)
                 .alignment(Alignment::Left),
-            inner,
+            light_inner,
         );
 
         frame.render_widget(
             Paragraph::new("«")
                 .style(text_style)
                 .alignment(Alignment::Right),
-            inner,
+            light_inner,
         );
     }
+
+    frame.render_widget(
+        Paragraph::new("I dont recommend this")
+            .style(description_style)
+            .alignment(Alignment::Center),
+        light_description,
+    );
+
+    // Back
+    let back_block = Block::bordered()
+        .border_type(BorderType::Double)
+        .border_style(if ui_state.submenu_hovered == Some(2) {
+            Style::default()
+                .fg(colors.selected)
+                .add_modifier(Modifier::BOLD)
+        } else {
+            border_style
+        });
+
+    frame.render_widget(back_block.clone(), back);
+
+    let back_inner = back_block.inner(back);
+
+    frame.render_widget(
+        Paragraph::new("Back")
+            .style(text_style)
+            .alignment(Alignment::Center),
+        back_inner,
+    );
+
+    if ui_state.submenu_hovered == Some(2) {
+        frame.render_widget(
+            Paragraph::new("»")
+                .style(text_style)
+                .alignment(Alignment::Left),
+            back_inner,
+        );
+
+        frame.render_widget(
+            Paragraph::new("«")
+                .style(text_style)
+                .alignment(Alignment::Right),
+            back_inner,
+        );
+    }
+
+    frame.render_widget(
+        Paragraph::new("Return to the previous menu")
+            .style(description_style)
+            .alignment(Alignment::Center),
+        back_description,
+    );
 }
 
 fn draw_banner(frame: &mut Frame, area: Rect, ui_state: &UiState) {
