4906483c8bcaa10b4dbc98335dbb1138f267d069
Sanju Sabu <sanjusabu.github@gmail.com>
2026-08-22 11:50:09 +0530

    Finish the key handling of gc and private chat submenu options

--- src/states.rs
+++ src/states.rs
@@ -1,2 +1,3 @@
 pub mod group_chat_state;
+pub mod private_chat_state;
 pub mod ui_state;
--- src/states/ui_state.rs
+++ src/states/ui_state.rs
@@ -8,7 +8,6 @@ use crate::{
 pub struct UiState {
     pub theme: Theme,
     pub username: String,
-    pub local_ip: String,
 
     pub in_home: bool,
     pub in_submenu: bool,
@@ -30,11 +29,18 @@ pub struct UiState {
 
     pub error: Option<ErrorKind>,
 
+    // IP address
+    pub local_ip: String,
+    pub local_ip_prefix: String,
+
     // input state
     pub input_mode: Option<InputMode>,
     pub input: String,
 
     pub previous_text: String,
+
+    // group chat
+    pub gc_mode: Option<GroupChatMode>,
 }
 
 #[derive(PartialEq)]
@@ -46,11 +52,6 @@ pub enum HomeOptions {
     Themes,
 }
 
-pub enum InChat {
-    Private,
-    Group,
-}
-
 #[derive(PartialEq, Clone, Copy)]
 pub enum InputMode {
     ChangeUsername,
@@ -58,11 +59,16 @@ pub enum InputMode {
     GroupChat,
 }
 
-// Enum that i should consider doing some changes
+#[derive(Clone, Copy)]
+pub enum InChat {
+    Private,
+    Group,
+}
+
+#[derive(Clone, Copy)]
 pub enum GroupChatMode {
     Client,
     Host,
-    Error(std::io::ErrorKind),
 }
 
 impl UiState {
@@ -70,12 +76,18 @@ impl UiState {
         let local_ip = if let Some(ip) = interface::get_local_ip() {
             ip
         } else {
-            String::from("Unknown")
+            String::from("UNKNOWN")
         };
+
+        let prefix = if let Some((take, _)) = local_ip.rsplit_once(".") {
+            take.to_string()
+        } else {
+            "UNKNOWN".to_string()
+        };
+
         Self {
             theme: Theme::Dark,
             username: username::default_username(),
-            local_ip,
 
             in_home: true,
             in_submenu: false,
@@ -97,10 +109,17 @@ impl UiState {
 
             error: None,
 
+            // IP address
+            local_ip,
+            local_ip_prefix: prefix,
+
             input: String::new(),
             input_mode: None,
 
             previous_text: String::new(),
+
+            // group chat
+            gc_mode: None,
         }
     }
 }
